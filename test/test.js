f48d9d633a511b73cecd38029481cf605bf91a75
ennahar <ennaharabdelhamid@gmail.com>
2014-11-17 16:22:05 +0100

    StoryOne

--- test/test.js
+++ test/test.js
@@ -3,4 +3,7 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var paletto = new Engine();
+    paletto.init();
+    assertTrue(paletto.juxta());
 };
